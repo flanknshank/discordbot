3ec8add4746925aab1b070b73befe6b9c41d4509
EricFergs <135307549+EricFergs@users.noreply.github.com>
2023-10-16 15:47:49 -0700

    image testing

--- simplecommands.py
+++ simplecommands.py
@@ -16,6 +16,11 @@ def setup(bot):
         await ctx.send(stageinfo.get_turf())
 
     @bot.command()
+    async def image(ctx):
+        with open('wiper.png', 'rb') as fp:
+            await ctx.send(file=discord.File(fp, 'new_filename.png'))
+
+    @bot.command()
     async def embed(ctx):
         embed=discord.Embed(title="Turf rotation", description="This is an embed that will show how to build an embed and the different components", color=0x90EE90)
         embed.set_thumbnail(url='https://splatoon3.ink/assets/regular.64299513.svg')
