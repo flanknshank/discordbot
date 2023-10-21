7b0c2a043eb08d5606b3d04e15e46a691f0ccb25
EricFergs <135307549+EricFergs@users.noreply.github.com>
2023-10-21 13:43:28 -0700

    added dependencies and testing message editing

--- simplecommands.py
+++ simplecommands.py
@@ -3,6 +3,10 @@ import discord
 import stageinfo
 import image_manipulation
 import os
+import asyncio
+from PIL import Image
+import pytesseract
+
 
 def setup(bot):
     @bot.command()
@@ -38,7 +42,12 @@ def setup(bot):
         await ctx.send(file=discord.File(image_map))
         os.remove('final.png')
 
-
+    @bot.command()
+    async def edit(ctx):
+        message = await ctx.send("https://cdn.discordapp.com/attachments/1162519403401859263/1165385244166336602/barnacle.png?ex=6546a896&is=65343396&hm=dbec87d6d93d5abd06f0972385a323063f2830f8c057773207fb1968401a4330&")
+        await asyncio.sleep(2)
+        await message.edit(content="https://cdn.discordapp.com/attachments/1162519403401859263/1165389464269496331/031.png?ex=6546ac84&is=65343784&hm=d0c851533c793e0b497d0b235c98854f7ba3f83344e7c9cc7f0707c410ca74c8&")
+        
     @bot.command()
     async def embed(ctx):
         embed=discord.Embed(title="Turf rotation", description="This is an embed that will show how to build an embed and the different components", color=0x90EE90)
